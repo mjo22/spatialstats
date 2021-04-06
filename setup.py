74ee81faf4a80440fff7789720dcb00dbd76afca
mjo22 <mjobrien22@gmail.com>
2021-04-06 17:39:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def read(fname):
@@ -8,14 +8,21 @@ def read(fname):
 
 
 setup(
-    name='softstats',
-    version="0.0.0",
+    name='spatialstats',
+    version="1.0.0",
+    license="MIT",
     author="Michael O'Brien",
     author_email="mobrien-temp@flatironinstitute.org",
     keywords="soft-condensed biophysics bispectrum cupy",
-    description=(
-        "softstats is collection of statistical tools used to analyze multi-scale soft and biological matter systems."),
+    url="https://github.com/mjo22/spatialstats",
+    description="spatialstats is collection of statistical tools used to analyze the multi-scale structure of spatial fields and particle distributions.",
     long_description=read("README.md"),
-    packages=find_packages(),
+    packages=["spatialstats"],
+    install_requires=["numpy", "scipy", "numba"],
+    classifiers=["License :: OSI Approved :: MIT License",
+                 "Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8"
+                 ],
     zip_safe=False
 )
