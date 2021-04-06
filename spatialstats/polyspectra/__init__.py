b57cd102969645adb0ace9215f91ff27dbdac290
mjo22 <mjobrien22@gmail.com>
2021-04-06 17:21:43 -0400

    Renaming

--- spatialstats/polyspectra/__init__.py
+++ spatialstats/polyspectra/__init__.py
@@ -1,11 +1,11 @@
 
-import softstats
+import spatialstats
 
-if softstats.config.gpu is False:
+if spatialstats.config.gpu is False:
     from .powerspectrum import powerspectrum
     from .bispectrum import bispectrum
 else:
     from .cuda_powerspectrum import powerspectrum
     from .cuda_bispectrum import bispectrum
 
-del softstats
+del spatialstats
